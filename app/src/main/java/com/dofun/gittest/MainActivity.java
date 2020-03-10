059d5665a1b1347db5f166057f2845a0e02c3d90
nijiahuan <312385179@qq.com>
2020-03-10 16:27:49 +0800

    关联非空仓库

--- app/src/main/java/com/dofun/gittest/MainActivity.java
+++ app/src/main/java/com/dofun/gittest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a = 1;
+        String s = "ss";
         Log.i("init  @@@ ", "onCreate: ");
     }
 }
